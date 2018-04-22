cf2cc6d7b51dc970d6ff3c41db8c839b7445d71a
MolVlad <molodcov.vladik@gmail.com>
2018-04-22 17:58:55 +0300

    lol

--- Project/main.c
+++ Project/main.c
@@ -7,23 +7,21 @@
 #include "stm32f0xx_ll_spi.h"
 
 /*-----|PINS|-----
-
 PB0			сервопривод
 PB2			пьезопищалка
 PB1			динамик
+
+PB4-PB7			принимающие для клавиатурыы
 
 PC0			питание
 PC1			открыто
 PC2			закрыто
 PC3			ожидается пароль
 
-	клавиатура
-подающие сигнал
-PC13-PC15
-принимающие сигнал
+PC6-PC8			освещение
 
+PC13-PC15		подающие для клавиатуры
 
-освещение
 PA1 			светодиод тревоги
 PA2			светодиод тревоги
 
@@ -39,13 +37,16 @@ PA8-PA12
 */
 
 volatile unsigned int tim2_brightness;
+volatile unsigned int tim14_brightness;
 volatile int real_time = 1736;
 volatile int sys_tick = 0;
 volatile int LCD_Enabled = 0;
-char Password[] = "12**";
+char Password[] = "****";
+char True_Password[4];
 
-const unsigned int TIM2_PWM_FREQ = 100;
+const unsigned int TIM2_PWM_FREQ = 200;
 const unsigned int TIM2_CLOCK_FREQ = 10;
+const unsigned int TIM14_CLOCK_FREQ = 100;
 const int SAFE_NUM = 15;
 
 enum FRONT
@@ -56,33 +57,77 @@ enum FRONT
 
 enum STATE_SYSTEM
 {
-	SLEEP = 1,
 	CLOSED,
 	OPENED,
-	WAITING,
+	WAITING_TO_CLOSE,
+	WAITING_TO_OPEN,
 	WRONG,
 	FAULT,
-	ASSERT_FIRE,
 	ASSERT_BREAKING,
 	BLOCKED
+#ifdef advanced
+	ASSERT_FIRE,
+	SLEEP,
+#endif
 };
 
 enum STATE_SYSTEM state_system_cur = 0;
-enum STATE_SYSTEM state_system_new = SLEEP;
+enum STATE_SYSTEM state_system_new = OPENED;
+
+int sound_tick;
+
+int buttons[][4] =
+{
+	{0, 0, 0, 0},
+	{0, 0, 0, 0},
+	{0, 0, 0, 0}
+};
+
+enum KEYBOARD_STATE
+{
+	Click,
+	No
+};
+
+enum KEYBOARD_STATE keyboard = No;
+
+enum SOUND
+{
+	On,
+	Off
+};
+
+enum SOUND sound = Off;
+
+enum COLUMN
+{
+	LEFT = 0,
+	MIDDLE,
+	RIGHT
+};
+
+enum COLUMN column_embedded;
 
 void SystemClock_Config();
 void SPI_Config();
 void EnableClock();
-void OutPut_Config();
+void OI_Config();
 void TIM3_Config();
 void TIM2_Config();
 void TIM14_Config();
+void TIM15_Config();
 void Timers_Config();
+void EXTI_Config();
 
 void SysTick_Handler();
 void HardFault_Handler();
 void TIM2_IRQHandler();
 void TIM14_IRQHandler();
+void TIM15_IRQHandler();
+void EXTI4_15_IRQHandler();
+
+void AssertSound_On();
+void AssertSound_Off();
 
 void LED_AssertBlink_On();
 void LED_AssertBlink_Off();
@@ -98,15 +143,18 @@ void LED_Waiting_On();
 void LED_Waiting_Off();
 
 void ChangeState();
-void State_Fire();
 void State_Breaking();
 void State_Blocked();
 void State_Opened();
 void State_Closed();
 void State_Waiting();
-void State_Sleep();
 void State_Fault();
 void State_Wrong();
+
+#ifdef advanced
+void State_Fire();
+void State_Sleep();
+#endif
 
 void LCD_Enable();
 void LCD_Command();
@@ -123,20 +171,31 @@ void LCD_Print_Time();
 void LCD_Print_Lattice(int num_str);
 void LCD_Print_Assert(int num_str);
 void LCD_Print_Safe_Num();
-void LCD_Print_Password(char * Password);
+void LCD_Print_Password();
 
 void Open_The_Lock();
 void Close_The_Lock();
+
+void Enable_Left_Column();
+void Enable_Middle_Column();
+void Enable_Right_Column();
+int Get_Number();
+void Execute_The_Command(int num);
+void Add_To_Password(int num);
+void Confirm_Password();
+void Clear_Password();
+void Check_Password();
+void Save_Password();
 
 int main()
 {
 	SystemClock_Config();
 	EnableClock();
 	SPI_Config();
-	OutPut_Config();
+	OI_Config();
 	LCD_Enable();
 	LED_Power_On();
-
+	EXTI_Config();
 	Timers_Config();
 
 	ChangeState();
@@ -149,6 +208,15 @@ void Timers_Config()
 	TIM2_Config();
 	TIM3_Config();
 	TIM14_Config();
+	TIM15_Config();
+}
+
+void Clear_Password()
+{
+	int i;
+
+	for(i = 0; i < 4; i++)
+		Password[i] = '*';
 }
 
 void Close_The_Lock()
@@ -237,18 +305,16 @@ void LED_Waiting_Off()
 
 void LED_Lighting_On()
 {
-	LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_4);
-	LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_5);
 	LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_6);
 	LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_7);
+	LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_8);
 }
 
 void LED_Lighting_Off()
 {
-	LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_4);
-	LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_5);
 	LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_6);
 	LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_7);
+	LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_8);
 }
 
 void LED_AssertBlink_On()
@@ -265,15 +331,24 @@ void LED_AssertBlink_Off()
 		LL_TIM_DisableCounter(TIM2);
 }
 
+void AssertSound_On()
+{
+
+	LL_TIM_EnableCounter(TIM14);
+}
+
+void AssertSound_Off()
+{
+	LL_TIM_OC_SetCompareCH1(TIM14, 0);
+	LL_TIM_DisableCounter(TIM14);
+}
+
 void ChangeState()
 {
 	if(state_system_cur != state_system_new)
 	{
 		switch (state_system_new)
 		{
-			case SLEEP:
-				State_Sleep();
-				break;
 			case CLOSED:
 				State_Closed();
 				break;
@@ -286,18 +361,25 @@ void ChangeState()
 			case FAULT:
 				State_Fault();
 				break;
-			case ASSERT_FIRE:
-				State_Fire();
-				break;
 			case ASSERT_BREAKING:
 				State_Breaking();
 				break;
 			case BLOCKED:
 				State_Blocked();
 				break;
-			case WAITING:
+			case WAITING_TO_CLOSE:
+				State_Waiting();
+			case WAITING_TO_OPEN:
 				State_Waiting();
 				break;
+#ifdef advanced
+			case SLEEP:
+				State_Sleep();
+				break;
+			case ASSERT_FIRE:
+				State_Fire();
+				break;
+#endif
 		}
 
 		state_system_cur = state_system_new;
@@ -326,6 +408,7 @@ void LCD_Print_Lattice(int num_str)
 	LCD_Print(0x0c * 6, 0x02 * num_str, 45);	//#
 }
 
+#ifdef advanced
 void State_Fire()
 {
 	LED_Opened_Off();
@@ -333,6 +416,8 @@ void State_Fire()
 	LED_Waiting_Off();
 	LED_AssertBlink_On();
 	LED_Lighting_Off();
+
+	AssertSound_On();
 
 	LCD_Backlight_On();
 
@@ -350,6 +435,22 @@ void State_Fire()
 	LCD_Wait_Busy();
 }
 
+void State_Sleep()
+{
+	LED_Opened_Off();
+	LED_Closed_Off();
+	LED_Waiting_Off();
+	LED_AssertBlink_Off();
+	LED_Lighting_Off();
+
+	AssertSound_Off();
+
+	LCD_Clear();
+	LCD_Wait_Busy();
+	LCD_Backlight_Off();
+}
+#endif
+
 void State_Blocked()
 {
 	LED_Opened_Off();
@@ -357,6 +458,8 @@ void State_Blocked()
 	LED_Waiting_Off();
 	LED_AssertBlink_On();
 	LED_Lighting_Off();
+
+	AssertSound_Off();
 
 	LCD_Backlight_On();
 
@@ -382,6 +485,8 @@ void State_Breaking()
 	LED_Waiting_Off();
 	LED_AssertBlink_On();
 	LED_Lighting_Off();
+
+	AssertSound_On();
 
 	LCD_Backlight_On();
 
@@ -435,6 +540,8 @@ void State_Opened()
 	LED_AssertBlink_Off();
 	LED_Lighting_On();
 
+	AssertSound_Off();
+
 	LCD_Backlight_On();
 
 	LCD_Print_Time();
@@ -458,6 +565,8 @@ void State_Wrong()
 	LED_Waiting_Off();
 	LED_AssertBlink_Off();
 	LED_Lighting_Off();
+
+	AssertSound_Off();
 
 	LCD_Backlight_On();
 
@@ -484,6 +593,8 @@ void State_Closed()
 	LED_AssertBlink_Off();
 	LED_Lighting_Off();
 
+	AssertSound_Off();
+
 	LCD_Backlight_On();
 
 	LCD_Print_Time();
@@ -509,6 +620,8 @@ void State_Fault()
 	LED_AssertBlink_Off();
 	LED_Lighting_Off();
 
+	AssertSound_Off();
+
 	LCD_Backlight_On();
 
 	LCD_Print_Lattice(0);
@@ -532,11 +645,13 @@ void State_Waiting()
 {
 	Close_The_Lock();
 
-	LED_Opened_Off();
-	LED_Closed_Off();
 	LED_Waiting_On();
 	LED_AssertBlink_Off();
 	LED_Lighting_Off();
+
+	Clear_Password();
+
+	AssertSound_Off();
 
 	LCD_Backlight_On();
 
@@ -561,12 +676,12 @@ void State_Waiting()
 	LCD_Print(0x0c * 0, 0x02 * 2, 0);
 	LCD_Print(0x0c * 6, 0x02 * 2, 0);
 
-	LCD_Print_Password(Password);
+	LCD_Print_Password();
 
 	LCD_Wait_Busy();
 }
 
-void LCD_Print_Password(char * Password)
+void LCD_Print_Password()
 {
 	int i;
 
@@ -581,20 +696,6 @@ void LCD_Print_Password(char * Password)
 	LCD_Print(0x0c * 5 + 0x05,  0x02 * 2, 0);	//отступ
 }
 
-
-void State_Sleep()
-{
-	LED_Opened_Off();
-	LED_Closed_Off();
-	LED_Waiting_Off();
-	LED_AssertBlink_Off();
-	LED_Lighting_Off();
-
-	LCD_Clear();
-	LCD_Wait_Busy();
-	LCD_Backlight_Off();
-}
-
 void EnableClock()
 {
 	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
@@ -605,10 +706,11 @@ void EnableClock()
 	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM2);
 	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM3);
 	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM14);
+	LL_APB1_GRP2_EnableClock(LL_APB1_GRP2_PERIPH_TIM15);
 	LL_APB1_GRP2_EnableClock(LL_APB1_GRP2_PERIPH_SPI1);
 }
 
-void OutPut_Config()
+void OI_Config()
 {
 	LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_0, LL_GPIO_MODE_OUTPUT);	//питание
 	LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_1, LL_GPIO_MODE_OUTPUT);	//открыт
@@ -616,10 +718,9 @@ void OutPut_Config()
 	LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_3, LL_GPIO_MODE_OUTPUT);	//ожидает пароль
 
 	//освещение сейфа
-	LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_4, LL_GPIO_MODE_OUTPUT);
-	LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_5, LL_GPIO_MODE_OUTPUT);
 	LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_6, LL_GPIO_MODE_OUTPUT);
 	LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_7, LL_GPIO_MODE_OUTPUT);
+	LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_8, LL_GPIO_MODE_OUTPUT);
 
 	LL_GPIO_SetPinMode(GPIOA, LL_GPIO_PIN_3, LL_GPIO_MODE_OUTPUT);	//подсветка экрана
 	LL_GPIO_SetPinMode(GPIOA, LL_GPIO_PIN_4, LL_GPIO_MODE_OUTPUT);	//для экрана
@@ -644,21 +745,47 @@ void OutPut_Config()
 
 	LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_2, LL_GPIO_MODE_OUTPUT);	//для пьезопищалки
 
-	LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_1, LL_GPIO_MODE_OUTPUT);	//для динамика
+	LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_1, LL_GPIO_MODE_ALTERNATE);	//для динамика
+	LL_GPIO_SetAFPin_0_7(GPIOB, LL_GPIO_PIN_1, LL_GPIO_AF_0);
 
+	LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_4, LL_GPIO_MODE_INPUT);		//принимают сигналы от клавиатуры
+	LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_5, LL_GPIO_MODE_INPUT);
+	LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_6, LL_GPIO_MODE_INPUT);
+	LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_7, LL_GPIO_MODE_INPUT);
 
+	LL_GPIO_SetPinPull(GPIOB, LL_GPIO_PIN_4, LL_GPIO_PULL_DOWN);
+	LL_GPIO_SetPinPull(GPIOB, LL_GPIO_PIN_5, LL_GPIO_PULL_DOWN);
+	LL_GPIO_SetPinPull(GPIOB, LL_GPIO_PIN_6, LL_GPIO_PULL_DOWN);
+	LL_GPIO_SetPinPull(GPIOB, LL_GPIO_PIN_7, LL_GPIO_PULL_DOWN);
 
-
-	LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_13, LL_GPIO_MODE_OUTPUT);
+	LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_13, LL_GPIO_MODE_OUTPUT);		//подают сигналы для клавиатуры
 	LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_14, LL_GPIO_MODE_OUTPUT);
 	LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_15, LL_GPIO_MODE_OUTPUT);
 
 	LL_GPIO_SetPinPull(GPIOC, LL_GPIO_PIN_13, LL_GPIO_PULL_DOWN);
 	LL_GPIO_SetPinPull(GPIOC, LL_GPIO_PIN_14, LL_GPIO_PULL_DOWN);
 	LL_GPIO_SetPinPull(GPIOC, LL_GPIO_PIN_15, LL_GPIO_PULL_DOWN);
+}
 
-	LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_13);
+void EXTI_Config()
+{
+	LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTB, LL_SYSCFG_EXTI_LINE4);
+	LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTB, LL_SYSCFG_EXTI_LINE5);
+	LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTB, LL_SYSCFG_EXTI_LINE6);
+	LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTB, LL_SYSCFG_EXTI_LINE7);
 
+	LL_EXTI_EnableIT_0_31(LL_EXTI_LINE_4);
+	LL_EXTI_EnableIT_0_31(LL_EXTI_LINE_5);
+	LL_EXTI_EnableIT_0_31(LL_EXTI_LINE_6);
+	LL_EXTI_EnableIT_0_31(LL_EXTI_LINE_7);
+
+	LL_EXTI_EnableRisingTrig_0_31(LL_EXTI_LINE_4);
+	LL_EXTI_EnableRisingTrig_0_31(LL_EXTI_LINE_5);
+	LL_EXTI_EnableRisingTrig_0_31(LL_EXTI_LINE_6);
+	LL_EXTI_EnableRisingTrig_0_31(LL_EXTI_LINE_7);
+
+	NVIC_EnableIRQ(EXTI4_15_IRQn);
+	NVIC_SetPriority(EXTI4_15_IRQn, 1);
 }
 
 void TIM14_Config()
@@ -673,6 +800,13 @@ void TIM14_Config()
 	TIM_InitStruct.RepetitionCounter = 0;
 	LL_TIM_Init(TIM14, &TIM_InitStruct);
 
+	LL_TIM_OC_StructInit(&TIM14_OC_CH1);
+	TIM14_OC_CH1.OCMode = LL_TIM_OCMODE_PWM1;
+	TIM14_OC_CH1.OCState = LL_TIM_OCSTATE_ENABLE;
+	TIM14_OC_CH1.OCPolarity = LL_TIM_OCPOLARITY_HIGH;
+	TIM14_OC_CH1.CompareValue = LL_TIM_GetAutoReload(TIM14);
+	LL_TIM_OC_Init(TIM14, LL_TIM_CHANNEL_CH1, &TIM14_OC_CH1);
+
 	NVIC_EnableIRQ(TIM14_IRQn);
 	NVIC_SetPriority(TIM14_IRQn, 1);
 
@@ -681,12 +815,70 @@ void TIM14_Config()
 	LL_TIM_EnableCounter(TIM14);
 }
 
-void TIM14_IRQHandler()		//здесь пишем для динамика
+void TIM14_IRQHandler()
 {
-	LL_GPIO_TogglePin(GPIOB, LL_GPIO_PIN_1);
+	if (LL_TIM_IsActiveFlag_UPDATE(TIM14))
+	{
+		static enum FRONT tim14_front = RISING;
 
-	LL_TIM_ClearFlag_UPDATE(TIM14);
+		switch (tim14_front)
+		{
+			case RISING:
+				tim14_brightness ++;
+				if (tim14_brightness == TIM14_CLOCK_FREQ)
+					tim14_front = FALLING;
+				break;
+			case FALLING:
+				tim14_brightness--;
+				if (tim14_brightness == 0)
+					tim14_front = RISING;
+				break;
+			default:
+				while(1);
+		}
+
+		LL_TIM_OC_SetCompareCH1(TIM14, (LL_TIM_GetAutoReload(TIM14) + 1)*tim14_brightness/TIM14_CLOCK_FREQ);
+
+		LL_TIM_ClearFlag_UPDATE(TIM14);
+	}
 }
+
+void TIM15_Config()
+{
+	LL_TIM_InitTypeDef TIM_InitStruct;
+	LL_TIM_OC_InitTypeDef TIM15_OC_CH2;
+
+	TIM_InitStruct.Prescaler = __LL_TIM_CALC_PSC(SystemCoreClock / 2, 2000);	//внимание! костыль
+	TIM_InitStruct.CounterMode = LL_TIM_COUNTERMODE_UP;
+	TIM_InitStruct.Autoreload = 1;
+	TIM_InitStruct.ClockDivision = LL_TIM_CLOCKDIVISION_DIV1;
+	TIM_InitStruct.RepetitionCounter = 0;
+	LL_TIM_Init(TIM15, &TIM_InitStruct);
+
+	LL_TIM_OC_StructInit(&TIM15_OC_CH2);
+	TIM15_OC_CH2.OCMode = LL_TIM_OCMODE_PWM1;
+	TIM15_OC_CH2.OCState = LL_TIM_OCSTATE_ENABLE;
+	TIM15_OC_CH2.OCPolarity = LL_TIM_OCPOLARITY_HIGH;
+	TIM15_OC_CH2.CompareValue = LL_TIM_GetAutoReload(TIM15) / 2;
+	LL_TIM_OC_Init(TIM15, LL_TIM_CHANNEL_CH2, &TIM15_OC_CH2);
+
+	NVIC_EnableIRQ(TIM15_IRQn);
+	NVIC_SetPriority(TIM15_IRQn, 1);
+
+	LL_TIM_EnableIT_UPDATE(TIM15);
+
+	LL_TIM_EnableCounter(TIM15);
+}
+
+void TIM15_IRQHandler()
+{
+	if(sound == On)
+		LL_GPIO_TogglePin(GPIOB, LL_GPIO_PIN_2);
+
+	LL_TIM_ClearFlag_UPDATE(TIM15);
+}
+
+
 
 void TIM3_Config()
 {
@@ -752,7 +944,7 @@ void TIM2_IRQHandler()
 		switch (tim2_front)
 		{
 			case RISING:
-				tim2_brightness++;
+				tim2_brightness ++;
 				if (tim2_brightness == TIM2_CLOCK_FREQ)
 					tim2_front = FALLING;
 				break;
@@ -864,32 +1056,295 @@ void SystemClock_Config()
 
 	LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_4);
 
-	SysTick_Config(48000000/50000);
+	SysTick_Config(48000000/1000);
 	SystemCoreClock = 48000000;
 }
 
-int a[] = {OPENED, WAITING, FAULT, OPENED, WAITING, CLOSED, WAITING, WRONG, CLOSED, ASSERT_FIRE, CLOSED, ASSERT_BREAKING, CLOSED, BLOCKED, SLEEP};
-int i = 0;
+void EXTI4_15_IRQHandler()
+{
+	if(LL_EXTI_IsActiveFlag_0_31(LL_EXTI_LINE_4))
+	{
+		buttons[column_embedded][3] = 1;
+		keyboard = Click;
+		LL_EXTI_ClearFlag_0_31(LL_EXTI_LINE_4);
+	}
+
+	if(LL_EXTI_IsActiveFlag_0_31(LL_EXTI_LINE_5))
+	{
+		buttons[column_embedded][2] = 1;
+		keyboard = Click;
+		LL_EXTI_ClearFlag_0_31(LL_EXTI_LINE_5);
+	}
+
+	if(LL_EXTI_IsActiveFlag_0_31(LL_EXTI_LINE_6))
+	{
+		buttons[column_embedded][1] = 1;
+		keyboard = Click;
+		LL_EXTI_ClearFlag_0_31(LL_EXTI_LINE_6);
+	}
+
+	if(LL_EXTI_IsActiveFlag_0_31(LL_EXTI_LINE_7))
+	{
+		buttons[column_embedded][0] = 1;
+		keyboard = Click;
+		LL_EXTI_ClearFlag_0_31(LL_EXTI_LINE_7);
+	}
+}
+
+void Enable_Left_Column()
+{
+	column_embedded = LEFT;
+
+	LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_13);
+	LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_14);
+	LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_15);
+}
+
+void Enable_Middle_Column()
+{
+	column_embedded = MIDDLE;
+
+	LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_13);
+	LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_14);
+	LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_15);
+}
+
+void Enable_Right_Column()
+{
+	column_embedded = RIGHT;
+
+	LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_13);
+	LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_14);
+	LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_15);
+}
+
+enum NUMBER
+{
+	NOTHING = 0,
+	ZERO,
+	STAR,
+	LATTICE,
+	NUM_1,
+	NUM_2,
+	NUM_3,
+	NUM_4,
+	NUM_5,
+	NUM_6,
+	NUM_7,
+	NUM_8,
+	NUM_9
+};
+
+int Get_Number()
+{
+	enum NUMBER ret;
+	int count = 0;
+	int i, j;
+
+	for(i = 0; i < 3; i++)
+		for(j = 0; j < 4; j++)
+			if(buttons[i][j] == 1)
+			{
+				count++;
+				if(j == 3)
+					switch (i)
+					{
+						case 0:
+							ret = STAR;
+							break;
+						case 1:
+							ret = ZERO;
+							break;
+						case 2:
+							ret = LATTICE;
+							break;
+					}
+				if(j == 2)
+					switch (i)
+					{
+						case 0:
+							ret = NUM_7;
+							break;
+						case 1:
+							ret = NUM_8;
+							break;
+						case 2:
+							ret = NUM_9;
+							break;
+					}
+				if(j == 1)
+					switch (i)
+					{
+						case 0:
+							ret = NUM_4;
+							break;
+						case 1:
+							ret = NUM_5;
+							break;
+						case 2:
+							ret = NUM_6;
+							break;
+					}
+				if(j == 0)
+					switch (i)
+					{
+						case 0:
+							ret = NUM_1;
+							break;
+						case 1:
+							ret = NUM_2;
+							break;
+						case 2:
+							ret = NUM_3;
+							break;
+					}
+				buttons[i][j] = 0;
+			}
+
+	if(count > 1)
+	{
+		ret = NOTHING;
+		LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_9);
+	}
+
+	return ret;
+}
+
+void Add_To_Password(int num)
+{
+
+}
+
+void Check_Password()
+{
+
+}
+
+void Save_Password()
+{
+	int i;
+
+	for(i = 0; i < 4; i++)
+		True_Password[i] = Password[i];
+}
+
+void Confirm_Password()
+{
+	int i;
+	int count_stars = 0;
+
+	for(i = 0; i < 4; i++)
+		if(Password[i] == '*')
+			count_stars++;
+
+	if(state_system_cur == WAITING_TO_CLOSE)
+	{
+		if(count_stars)
+		{
+			state_system_new = FAULT;
+			ChangeState();
+		}
+		else
+		{
+			Save_Password();
+			state_system_new = CLOSED;
+			ChangeState();
+		}
+	}
+	else
+	{
+		if(count_stars)
+		{
+			state_system_new = WRONG;
+			ChangeState();
+		}
+		else
+			Check_Password();
+	}
+}
+
+void Execute_The_Command(int num)
+{
+	if(num == STAR)
+		switch (state_system_cur)
+		{
+			case OPENED:
+				state_system_new = WAITING_TO_CLOSE;
+				ChangeState();
+				break;
+			case CLOSED:
+				state_system_new = WAITING_TO_OPEN;
+				ChangeState();
+				break;
+			case WAITING_TO_CLOSE:
+				Confirm_Password();
+				break;
+			case WAITING_TO_OPEN:
+				Confirm_Password();
+				break;
+			default:
+				break;
+		}
+	else if(num == LATTICE)
+		switch (state_system_cur)
+		{
+			case WAITING_TO_CLOSE:
+				state_system_new = OPENED;
+				ChangeState();
+				break;
+			case WAITING_TO_OPEN:
+				state_system_new = CLOSED;
+				ChangeState();
+				break;
+			default:
+				break;
+		}
+	else if(num != NOTHING)
+		switch (state_system_cur)
+		{
+			case WAITING_TO_CLOSE:
+				Add_To_Password(num);
+				break;
+			case WAITING_TO_OPEN:
+				Add_To_Password(num);
+				break;
+			default:
+				break;
+		}
+}
 
 void SysTick_Handler()
 {
-	sys_tick++;
-
-	if(LCD_Enabled)
+	if(sound_tick == 200)
 	{
-		if(sys_tick % 100000 == 0)
-		{
-			state_system_new = a[i];
-			ChangeState();
-			i++;
-			if(i == 16)
-				i = 0;
-		}
+		sound_tick = 0;
+		sound = Off;
+		keyboard = No;
+		int num = Get_Number();
+		Execute_The_Command(num);
 	}
 
-	if(sys_tick % 1000000 == 0)
+	if(sound == On)
+		sound_tick++;
+
+	if(keyboard == Click && sound == Off)
+	{
+		sound = On;
+	}
+
+	if(sys_tick == 1000)
 		sys_tick = 0;
 
+	if(sys_tick % 3 == 0)
+		Enable_Left_Column();
+
+	if(sys_tick % 3 == 1)
+		Enable_Middle_Column();
+
+	if(sys_tick % 3 == 2)
+		Enable_Right_Column();
+
+	sys_tick++;
 }
 static const char LCD_Base[][24] =
 {
